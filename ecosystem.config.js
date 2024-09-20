18a73c5c791512306edf0c8748bff7d26aa14159
mesepith <zahiralam29@gmail.com>
2024-09-20 12:03:17 +0530

    pm2 setup min

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       instances: "max", // This will use all CPUs. Adjust as needed.
       autorestart: true,
       watch: false, // Set to true if you want PM2 to restart on file changes
-      max_memory_restart: '500MB',
+      max_memory_restart: '500M',
       dotenv: '.env'
     }]
   };
